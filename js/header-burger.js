cc4073787143f0c868a2d5c48009843abc704d95
RomanNahaievskiy <nagaewsky.roma.s@gmail.com>
2023-03-29 19:38:34 +0300

    ++

--- js/header-burger.js
+++ js/header-burger.js
@@ -10,6 +10,7 @@ window.addEventListener('click', function (e) {
         document.body.classList.toggle('lock');
     }
     if (e.target.closest('.lock')) {
+        // тут краще додати клас для підложки а вже в scss прописати ці стилі для нього
         headerBackground.style.cssText = `
         position: absolute;
         top: 0;
@@ -19,6 +20,7 @@ window.addEventListener('click', function (e) {
         z-index: 2;
         background-color: #fff;
         transition: all 0.1s ease 0s;
+        
         `;
     } else {
         headerBackground.style.cssText = `
@@ -33,7 +35,7 @@ window.addEventListener('click', function (e) {
         `;
     }
 })
-// заготовка до прозорого фону , коли миша на  хедері
+// заготовка до прозорого фону , коли миша на  хедері ще потрібні правки
 window.addEventListener('mouseover', function (e) {
     if (e.target.closest('.header')) {
         headerBackground.style.cssText = `
@@ -41,10 +43,11 @@ window.addEventListener('mouseover', function (e) {
         top: 0;
         left: 0;
         width: 100%;
-        height: 86px;
+        height: 94px;
         z-index: 2;
-        background-color: #ffffff8f;
+        background-color: #ffffffaa;
         transition: all 0.1s ease 0s;
+        
         `;
     } else {
         headerBackground.style.cssText = `
@@ -52,10 +55,49 @@ window.addEventListener('mouseover', function (e) {
         top: 0;
         left: 0;
         width: 100%;
-        height: 86px;
+        height: 94px;
         z-index: 2;
         background-color: transparent;
         transition: all 0.6s ease 0s;
-        `;
+                `;
     }
 })
+//========================================================================================================================================================
+//SCROLL event
+
+
+const arrScroll = [];// cтворив масив значень скролу для відлову попередніх значень (потрібно придумати , як його очищувати , якщо там більше ніж 2 елементи)
+window.addEventListener('scroll', (e) => {
+    arrScroll.push(scrollY);
+    // (обмежую масив до 2х останніх свіжих значень) є думка обмежувати до 10 і  в результаті різниці значень впливати на затримку появи шапки .
+    if (arrScroll.length > 2) {
+        arrScroll.splice(0, 1);
+    }
+
+    // let prevScroll = arrScroll[arrScroll.length - 2];
+    // let lastScroll = arrScroll[arrScroll.length - 1];
+    // трішки оптимізації не завадить
+    let prevScroll = arrScroll[0];
+    let lastScroll = arrScroll[1];
+
+    // якщо скролимо вниз то ховаємо шапочку меню
+    if (lastScroll > prevScroll) {
+        //todo заставити сховатися шапку
+        // звертаюся до шапки  покласу і додаю ще один клас  ._scroll-down , або можна додавати  якийсь дата-атрибут 
+        // даний клас ще опишу в файлі стилів 
+        header.classList.add('_scroll-down');
+
+    }
+    // якщо скролимо вгору то показуємо шапочку меню
+    else {   //if (lastScroll < prevScroll)
+        //todo заставити показатися шапку
+        // звертаюся до шапки  покласу і видаляю клас  ._scroll-down 
+        header.classList.remove('_scroll-down');
+    }
+
+
+
+}
+)
+
+
